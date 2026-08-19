17f0af578a739b8a8e8ce4bf70ac0bd6dd66a6d4
Laurin Quast <laurinquast@googlemail.com>
2026-08-19 17:50:51 +0200

    lets run on all supported versions :eyes:

--- vite.config.ts
+++ vite.config.ts
@@ -2,31 +2,53 @@ import { join } from 'path';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 
-export default defineConfig({
-  resolve: {
-    alias: [
-      {
-        find: /^graphql$/,
-        replacement: join(__dirname, 'node_modules/graphql/index.mjs'),
-      },
-    ],
-  },
-  test: {
-    globals: true,
-    alias: {
-      '@graphql-inspector/commands': 'packages/commands/commands/src/index.ts',
-      '@graphql-inspector/loaders': 'packages/loaders/loaders/src/index.ts',
-      '@graphql-inspector/logger': 'packages/logger/src/index.ts',
-      '@graphql-inspector/url-loader': 'packages/loaders/url/src/index.ts',
-      '@graphql-inspector/testing': 'packages/testing/src/index.ts',
-      '@graphql-inspector/core': 'packages/core/src/index.ts',
-      '@graphql-inspector/patch': 'packages/patch/src/index.ts',
+export default defineConfig(({ mode }) => {
+  const usesLegacyGraphQL = /^graphql-(?:14|15|16)$/.test(mode);
+  const graphqlPackage = usesLegacyGraphQL ? mode : 'graphql';
+
+  return {
+    resolve: {
+      alias: [
+        {
+          find: /^graphql$/,
+          replacement: join(__dirname, `node_modules/${graphqlPackage}/index.mjs`),
+        },
+        ...(usesLegacyGraphQL
+          ? [
+              {
+                find: /^graphql\/(.*)$/,
+                replacement: join(__dirname, `node_modules/${graphqlPackage}/$1`),
+              },
+            ]
+          : []),
+      ],
     },
-    setupFiles: ['./packages/testing/src/setup-file.ts'],
-  },
-  plugins: [
-    tsconfigPaths({
-      projects: [join(__dirname, 'tsconfig.test.json')],
-    }),
-  ],
+    test: {
+      globals: true,
+      alias: {
+        '@graphql-inspector/commands': 'packages/commands/commands/src/index.ts',
+        '@graphql-inspector/loaders': 'packages/loaders/loaders/src/index.ts',
+        '@graphql-inspector/logger': 'packages/logger/src/index.ts',
+        '@graphql-inspector/url-loader': 'packages/loaders/url/src/index.ts',
+        '@graphql-inspector/testing': 'packages/testing/src/index.ts',
+        '@graphql-inspector/core': 'packages/core/src/index.ts',
+        '@graphql-inspector/patch': 'packages/patch/src/index.ts',
+      },
+      setupFiles: ['./packages/testing/src/setup-file.ts'],
+      ...(usesLegacyGraphQL
+        ? {
+            server: {
+              deps: {
+                inline: true,
+              },
+            },
+          }
+        : {}),
+    },
+    plugins: [
+      tsconfigPaths({
+        projects: [join(__dirname, 'tsconfig.test.json')],
+      }),
+    ],
+  };
 });
